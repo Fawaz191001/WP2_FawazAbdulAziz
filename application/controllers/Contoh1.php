08fddcaf3ab18f1ab8d7af09af0c8fdfa5f82ed8
Fawaz191001 <fawazbmc191001@gmail.com>
2022-03-21 19:37:47 +0700

    Menambahkan isi contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Fawaz Abdul Aziz
+ Saya tingga di daerah Kp.Ketapang Kota Tangerang
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
