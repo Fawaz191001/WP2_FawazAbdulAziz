ba88054e49b51e4196b3501e1143c3b4977b13a5
Fawaz191001 <fawazbmc191001@gmail.com>
2022-03-30 14:10:13 +0700

    Menambahkan Pertemuan 4 yaitu = web.php di application/controllers,v_about.php di application/views,v_footer.php di application/views,v_header.php di application/views,v_index.php di application/views,stylebuku.css di assets/css

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Fawaz</li>
+ <li>Nama Belakang : Aziz</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Kampung Ketapang Tanggerang</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Kuningan</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Catur</li>
+ </ul>
+</section>
